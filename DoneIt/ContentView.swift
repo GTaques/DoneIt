3b66f2dd1b954f3f19998fcacda9162eb7eefc1b
Gabriel Taques <gabrielpctaques@gmail.com>
2020-03-02 09:46:32 -0300

    Initial Commit

---
+++ DoneIt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoneIt
+//
+//  Created by Gabriel Taques on 02/03/20.
+//  Copyright © 2020 Gabriel Taques. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
